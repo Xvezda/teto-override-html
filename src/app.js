fb6dee4c598e47f184353f7dbb035264e2db0df1
Xvezda <xvezda@naver.com>
2024-10-19 11:26:10 +0900

    Add key binding feature

--- src/app.js
+++ src/app.js
@@ -1,38 +1,41 @@
 window.onload = function() {
   let currentScene = 0;
   const scenes = [
-    'scene-1',
-    'scene-1',
-    'scene-1',
-    'scene-1',
-    'scene-2',
-    'scene-3',
-    'scene-2',
-    'scene-3',
-    'scene-2',
-    'scene-3',
-    'scene-2',
-    'scene-3',
-    'scene-4',
-    'scene-4',
-    'scene-4',
-    'scene-4',
-    'scene-2',
-    'scene-3',
-    'scene-2',
-    'scene-3',
-    'scene-2',
-    'scene-3',
-    'scene-2',
-    'scene-3',
+    'scene-1', 'scene-1', 'scene-1', 'scene-1',
+    'scene-2', 'scene-3', 'scene-2', 'scene-3',
+    'scene-2', 'scene-3', 'scene-2', 'scene-3',
+    'scene-4', 'scene-4', 'scene-4', 'scene-4',
+    'scene-2', 'scene-3', 'scene-2', 'scene-3',
+    'scene-2', 'scene-3', 'scene-2', 'scene-3',
+    'scene-5', 'scene-6', 'scene-5', 'scene-6',
+    'scene-5', 'scene-6', 'scene-5', 'scene-6',
+    'scene-2', 'scene-3', 'scene-2', 'scene-3',
+    'scene-2', 'scene-3', 'scene-2', 'scene-3',
+    'scene-1', 'scene-1', 'scene-1', 'scene-1',
   ];
 
   const nextScene = () => {
-    currentScene++;
-    document.body.className = scenes[currentScene % scenes.length];
+    const scene = scenes[++currentScene % scenes.length];
+    if (document.body.className !== scene) {
+      document.body.className = scene;
+    }
   };
 
-  setInterval(nextScene, 300 * 4);
+  let interval = setInterval(nextScene, 300 * 4);
+
+  window.addEventListener('keydown', (e) => {
+    switch (e.key) {
+      case '1': case '2': case '3': case '4': case '5': case '6':
+        clearInterval(interval);
+        document.body.className = `scene-${e.key}`;
+        break;
+      case '0':
+        setInterval(nextScene, 300 * 4);
+        break;
+      default:
+        break;
+    }
+  });
 
   const audioButton = document.getElementById('audio');
   audioButton.addEventListener('click', () => {
